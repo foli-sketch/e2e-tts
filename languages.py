b55a01ffa39c06d2baf5440e5ddd6b5784eb460d
foli-sketch <somchinmoy5@gmail.com>
2026-02-19 21:13:34 +0530

    Add TTS_LANGUAGES dictionary for language support

---
+++ languages.py
@@ -0,0 +1,12 @@
+TTS_LANGUAGES = {
+    "en": "English",
+    "bn": "Bengali",
+    "hi": "Hindi",
+    "fr": "French",
+    "de": "German",
+    "es": "Spanish",
+    "it": "Italian",
+    "pt": "Portuguese",
+    "ru": "Russian",
+    "zh": "Chinese"
+}
